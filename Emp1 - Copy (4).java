0e7cfa520f85c637131be045f6b53d0dc0e24e82
suvarnajadhav1 <suvarnajadhav4882@gmail.com>
2023-08-23 22:18:20 +0530

    Employeeinfo

---
+++ Emp1 - Copy (4).java
@@ -0,0 +1,28 @@
+class Emp1
+{
+	public static void main(String[] arg)
+	{
+	int Empid;
+	String Empname;
+	double Empsalary;
+	long Contact;
+	boolean iffresher;
+edrftgyhujikoldrfgyhujikolp;[sxcfvgbhnjmk,l.;vbghn;fvgbhunjmiko,lp.;
+
+	Empid=12345;
+    Empname="kiran";
+    Empsalary=23000.00;
+    Contact=234567891234l;
+    iffresher=true;
+
+    System.out.println("Empid:"+Empid);
+     System.out.println("Empname:"+Empname);
+      System.out.println("Empsalary:"+Empsalary);
+       System.out.println("Contact:"+Contact);
+       System.out.println("iffresher:"+iffresher);
+
+
+
+
+	}
+}
